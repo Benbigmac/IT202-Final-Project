c17fdb2280e233802b7716663f0e3730dd3fbb96
Bennett Maciorowski <bmacio2@uic.edu>
2018-12-02 07:29:19 +0000

    added Voice stuff

---
+++ js/dbStuff.js
@@ -0,0 +1,20 @@
+function add(){ db.friends.put({name: "Nicolas", shoeSize: 8}).then (function(){
+              //
+              // Then when data is stored, read from it
+              //
+              return db.friends.get('Nicolas');
+          }).then(function (friend) {
+              //
+              // Display the result
+              //
+              alert ("Nicolas has shoe size " + friend.shoeSize);
+          }).catch(function(error) {
+             //
+             // Finally don't forget to catch any error
+             // that could have happened anywhere in the
+             // code blocks above.
+             //
+             alert ("Ooops: " + error);
+          });
+
+}
--- js/voice.js
+++
@@ -1,4 +0,0 @@
-function aboutMe(){
-   var utterthis = new SpeechSynthesisUtterance("THis is some stuff about me");
- window.speechSynthesis.speak(utterthis); 
-} 
